7a132553b5e1eeba297995119c7247b0b562ad83
Harrison Goldstein <hgoldstein95@gmail.com>
2025-12-18 09:50:20 -0500

    Update to 4.27.0-rc1 and add mathlib

--- Basalt/IO.lean
+++ Basalt/IO.lean
@@ -6,5 +6,4 @@ open RandomChoice
 instance : RandomChoice IO where
   choose lo hi _ := IO.rand lo hi
 
--- FIXME: Once Lean updates, this shouldn't need to be unsafe.
-unsafe abbrev Gen.runIO (g : Gen α) : IO α := @g _ _ (unsafeCast ()) _ (unsafeCast ()) _
+def Gen.runIO (g : Gen α) : IO α := g
