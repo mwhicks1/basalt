61d3920b321007416f2a2e8fcfb38c3afc7efed1
Harrison Goldstein <hgoldstein95@gmail.com>
2026-01-02 15:35:08 -0500

    More documentation

--- Basalt/Gen.lean
+++ Basalt/Gen.lean
@@ -2,6 +2,23 @@ import Basalt.RandomChoice
 
 open Lean.Order
 
+/-!
+This file defines the top-level interface of PBT generators.
+-/
+
+/-- Our representation of generators abstracts over a number of type-classes, each of which provides
+  some important set of generator operations:
+- `Monad`: Generators compose monadically.
+- `RandomChoice`: Generators can express discrete random choices.
+- `Inhabited`: Generators can fail; there is a bottom value that represents non-termination and
+  other failures.
+- `CCPO` and `MonoBind`: Generators are partially-ordered and admit Knaster-Tarski fixed-points.
+  Generators that do not terminate structurally, or at all, are still expressable. (The monad's bind
+  operation should respect the partial order.)
+
+NOTE: I'm not sure this is actually right. This doesn't work well with higher-order generators, and
+it also makes `Gen` a `Type → Type 1`, which feels odd.
+-/
 def Gen (α : Type) :=
   ∀ {m : Type → Type}
     [∀ α, Inhabited (m α)]
