6db83cd49d3e6dba9781efc327ca64d017712ccd
Harrison Goldstein <hgoldstein95@gmail.com>
2026-03-25 23:10:07 -0400

    Add support proofs for sorted list

--- Basalt/Examples/SortedList.lean
+++ Basalt/Examples/SortedList.lean
@@ -30,11 +30,65 @@ def List.sorted (xs : List Nat) : Prop :=
   | [_] => True
   | x :: y :: xs => x <= y ∧ List.sorted (y :: xs)
 
+lemma List.sorted_le : List.sorted (x :: xs) → List.Forall (x ≤ ·) xs := by
+  intro h
+  induction xs
+  case _ => simp
+  case _ x xs ih => grind [= sorted.eq_def, sorted, List.forall_cons]
+
+theorem List.genSortedGt_support (xs : List Nat) (m : Nat) :
+    xs ∈ SPMF.support (List.genSortedGt m) ↔ (List.sorted xs ∧ List.Forall (m ≤ ·) xs) := by
+  fun_induction List.sorted generalizing m
+  case _ =>
+    unfold genSortedGt
+    simp
+  case _ x =>
+    unfold genSortedGt
+    simp
+    constructor
+    . grind
+    . intro h
+      exists x - m
+      apply And.intro Nat.arbitrary_support
+      constructor
+      . unfold genSortedGt
+        simp
+      . grind
+  case _ x y xs ih =>
+    unfold genSortedGt
+    simp only [bind_pure_comp, SPMF.support_pick, SPMF.support_pure, SPMF.support_bind,
+      SPMF.support_map, Set.mem_setOf_eq, Set.singleton_union, Set.mem_insert_iff, reduceCtorEq,
+      List.cons.injEq, exists_eq_right_right', false_or, List.Forall, List.forall_cons]
+    constructor
+    . intro h
+      replace ⟨a, _, h, heq⟩ := h
+      subst heq
+      grind only [List.forall_iff_forall_mem, List.forall_cons]
+    . intro h
+      replace ⟨h, hx, hy, hxs⟩ := h
+      exists x - m
+      grind only [
+        List.forall_iff_forall_mem, List.Forall.eq_def, List.Forall.imp, List.sorted_le,
+        sorted.eq_def, Nat.arbitrary_support]
+
+theorem List.genSorted_support (xs : List Nat) :
+    xs ∈ SPMF.support List.genSorted ↔ List.sorted xs := by
+  unfold genSorted
+  simp [genSortedGt_support]
+  constructor
+  . grind
+  . intro h
+    exists 0
+    constructor
+    . simp [Nat.arbitrary_support]
+    . apply And.intro h
+      simp [List.forall_iff_forall_mem]
+
 -- TODO: Complete these sorrys
 /-- warning: declaration uses `sorry` -/
 #guard_msgs in
 instance : LawfulGenerator List.genSorted List.sorted List.genSorted.costBound where
-  is_correct := sorry
+  is_correct := by simp [List.genSorted_support]
   is_ast := sorry
   is_cost_bounded := sorry
 
